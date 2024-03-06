ccc40331e28e6bfa621126cd25402a9a39be2a82
Willian-Girao <williansoaresgirao@gmail.com>
2024-03-06 23:18:46 +0100

    training with spike data

--- FMNIST2spikes.py
+++ FMNIST2spikes.py
@@ -8,147 +8,122 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import torch
-import torchvision
 
-from CSNN import CSNN
+def current2firing_time(x, tau_mem=20.0, thr=0.2, tmax=100, epsilon=1e-7):
+    """
+    Computes first spiking time latency for a current input x ('current injection') fed to a LIF neuron (current-based LIF).
 
-def main():
+    Arguments:
+    - tau_mem: membrane time constant of LIF neuron.
+    - thr: membrane's spike threshold
+    - tmax: maximum time returned (neurons that did not fire)
 
-    if torch.cuda.is_available():               # check GPU availability
-        device = torch.device('cuda')
-    else:
-        device = torch.device('cpu')
+    Returns:
+    - T: time to first spike given each "current injection" represented in x.
+    """
 
-    ### 1. DATA LOADING ###
+    idx = x < thr                           # neurons that did not fire a spike
+    x = np.clip(x, thr+epsilon, 1e9)        # prevents invalid computations in the subsequent logarithmic calculation
+    T = tau_mem*np.log(x/(x-thr))           # time mem takes to reach thr given input 'x' and time constant 'tau' for current-based LIF neuron
+    T[idx] = tmax                           # neurons that did not fire a spike take the longest to first spike
+
+    return  np.array(T, dtype=int)
+
+def sparse_data_gen(X, Y, batch_size: int, device, shuffle=False, num_units=(28, 28), time_step=1e-3, num_steps=100, tau_mem=20e-3):
+    """
+    Take a dataset in analog (continuous value) format and generates spikes tensors.
+
+    Arguments:
+    - batch_size: number of sample in each batch
+    - shuffle: shuffle samples during batch creation
+    - num_units: dimensions of image
+    - time_step: time taken by a single simulated time step (how finely time is discretized)
+    - num_steps: number of discrete time steps simulated (each representing 'time_step' units of time)
+    - tau_mem: membrane time constant (continuous-time)
+
+    Returns:
+    - X_batch: three dimensional tensor (# of sample in the batch, # of discrete time steps, # of units) containing spike representation of images dataset
+    - Y_batch: the labels
+    """
+
+    labels_ = np.array(Y, dtype=int)
+    num_batches = len(X)//batch_size
+    sample_index = np.arange(len(X))
+
+    if shuffle:
+        np.random.shuffle(sample_index)
+
+    # compute discrete firing times
         
-    # --- 1.1. loading Fashion MNIST dataset ---
-        
-    batch_size = 1
-    root = 'datasets'
+    tau_mem_adjusted = tau_mem/time_step    # scaling (continuous-time) 'tau_mem' to fit the simulation's (discrete-time) step
+
+    firing_times = current2firing_time(X, 
+                                        tau_mem=tau_mem_adjusted, 
+                                        tmax=num_steps)
     
-    train_dataset = torchvision.datasets.FashionMNIST(root, train=True, transform=None, target_transform=None, download=True)
-    test_dataset = torchvision.datasets.FashionMNIST(root, train=False, transform=None, target_transform=None, download=True)
+    units_ids = np.indices(num_units)
 
-    # --- 1.2. standardizing dataset ---
+    counter = 0
 
-    x_train = np.array(train_dataset.data, dtype=float)/255
-    y_train = np.array(train_dataset.targets, dtype=int)
+    while counter < num_batches:                                                    # building i-th batch of spiking data
 
-    x_test = np.array(test_dataset.data, dtype=float)/255
-    y_test = np.array(test_dataset.targets, dtype=int)
+        batch_index = sample_index[batch_size*counter:batch_size*(counter+1)]       # gathering samples belonging to the i-th batch
 
-    print(f'x_train shape: {x_train.shape}')
-    print(f'x_test shape: {x_test.shape}')
+        coo_ = [ [] for i in range(4) ]                                             # matrix holding analog-latency conversion results of all samples within the i-th batch
 
-    print(f'y_train shape: {y_train.shape}')
-    print(f'y_test shape: {y_test.shape}')
+        for enumerator, sample_idx in enumerate(batch_index):
 
-    # --- 1.3. analog to spike data ---
+            c = firing_times[sample_idx] < num_steps                                # getting units that will fire within 'num_steps' simulation steps
 
-    def current2firing_time(x, tau_mem=20.0, thr=0.2, tmax=100, epsilon=1e-7):
+            spks_tsteps, x_coor, y_coor = firing_times[sample_idx][c], units_ids[0][c], units_ids[1][c]
+
+            batch_sample = [ enumerator for _ in range(len(spks_tsteps)) ]          # within batch sample's index (same for each unit that will spike) - to what sample in the batch each unit belongs to
+
+            coo_[0].extend(batch_sample)                                            # sample dimension indexing
+            coo_[1].extend(spks_tsteps)                                             # tstep dimension indexing
+            coo_[2].extend(x_coor)                                                  # image's grid x-coordinate indexing
+            coo_[3].extend(y_coor)                                                  # image's grid y-coordinate indexing
+
         """
-        Computes first spiking time latency for a current input x ('current injection') fed to a LIF neuron (current-based LIF).
+            In the following lines of code are creatig sparse tensors for improved memory usage (source: https://pytorch.org/docs/stable/sparse.html, 
+        section "Construction"). They will effectively transform the latency encoding above into spiking data.
 
-        Arguments:
-        - tau_mem: membrane time constant of LIF neuron.
-        - thr: membrane's spike threshold
-        - tmax: maximum time returned (neurons that did not fire)
+        The tensor 'X_batch' will have three dimensions (a - # of sample in the batch, b - # of discrete time steps, c - # of units). This means that, for each of
+        the 'num_steps' discrete time steps, a unit 'c' in sample 'a' will have a '1' at X_batch[a][b][c] if at time_step 'b' it was above the thr value (it will be
+        '0' otherwise).
 
-        Returns:
-        - T: time to first spike given each "current injection" represented in x.
+        The data becomes "spiking" data because the 2nd argument of 'torch.sparse.FloatTensor' sets the entries (values) for 'X_batch' that are not supposed to be
+        the default (zero) ones.
+
+        The 3rd argument of 'torch.sparse.FloatTensor' defines the dimensions of the resulting tensor (3D in this case, as explined above). This means that each
+        sample (1st dim.) is represented in a span of 'num_steps' (2nd dim.) discrete time by 'num_units' (3rd dim.) spiking units. The computed time-to-first-spike
+        gives in which of the 'num_steps' the unit will be presented as having spiked.
         """
-
-        idx = x < thr                           # neurons that did not fire a spike
-        x = np.clip(x, thr+epsilon, 1e9)        # prevents invalid computations in the subsequent logarithmic calculation
-        T = tau_mem*np.log(x/(x-thr))           # time mem takes to reach thr given input 'x' and time constant 'tau' for current-based LIF neuron
-        T[idx] = tmax                           # neurons that did not fire a spike take the longest to first spike
-
-        return  np.array(T, dtype=int)
-
-    def sparse_data_gen(X, Y, batch_size: int, device, shuffle=False, num_units=(28, 28), time_step=1e-3, num_steps=100, tau_mem=20e-3):
-        """
-        Take a dataset in analog (continuous value) format and generates spikes tensors.
-
-        Arguments:
-        - batch_size: number of sample in each batch
-        - shuffle: shuffle samples during batch creation
-        - num_units: dimensions of image
-        - time_step: time taken by a single simulated time step (how finely time is discretized)
-        - num_steps: number of discrete time steps simulated (each representing 'time_step' units of time)
-        - tau_mem: membrane time constant (continuous-time)
-
-        Returns:
-        - X_batch: three dimensional tensor (# of sample in the batch, # of discrete time steps, # of units) containing spike representation of images dataset
-        - Y_batch: the labels
-        """
-
-        labels_ = np.array(Y, dtype=int)
-        num_batches = len(X)//batch_size
-        sample_index = np.arange(len(X))
-
-        if shuffle:
-            np.random.shuffle(sample_index)
-
-        # compute discrete firing times
-            
-        tau_mem_adjusted = tau_mem/time_step    # scaling (continuous-time) 'tau_mem' to fit the simulation's (discrete-time) step
-
-        firing_times = current2firing_time(X, 
-                                           tau_mem=tau_mem_adjusted, 
-                                           tmax=num_steps)
         
-        units_ids = np.indices(num_units)
+        indices = torch.tensor(coo_, dtype=torch.int64)                             # [[batch samples], [total time steps], [input gridcell x axis], [input gridcell y axis]]
+        values = torch.tensor(np.ones(len(coo_[0])), dtype=torch.float32)           # spike as 1.0 at time step t (otherwise 0.0 default value)
+        
+        X_batch = torch.sparse_coo_tensor(indices, values, (batch_size, num_steps, num_units[0], num_units[0])).to(device=device)
+        Y_batch = torch.tensor(labels_[batch_index], device=device)
 
-        counter = 0
+        # Uncomment lines bellow to export the "spiking image" for the samples.
+        # w = X_batch.to_dense()
+        # wtolist = w.tolist()
+        # counts = 0
+        # for sample in wtolist:
+        #     counts += 1
+        #     countt = 0
+        #     for tstep in sample:
+        #         print(f'sample # {counts}, tstep: {countt}')
+        #         plt.title(f'sample # {counts}, tstep: {countt}')
+        #         plt.imshow(np.array(tstep), cmap=plt.cm.gray_r)
+        #         plt.axis('off')
+        #         plt.savefig(f'animation/tstep_{countt}.png')
+        #         plt.close()
+        #         countt += 1
 
-        while counter < num_batches:                                                    # building i-th batch of spiking data
+        # input('stopped (v2)...')
 
-            batch_index = sample_index[batch_size*counter:batch_size*(counter+1)]       # gathering samples belonging to the i-th batch
+        yield X_batch.to(device), Y_batch.to(device)
 
-            coo_ = [ [] for i in range(4) ]                                             # matrix holding analog-latency conversion results of all samples within the i-th batch
-
-            for enumerator, sample_idx in enumerate(batch_index):
-
-                c = firing_times[sample_idx] < num_steps                                # getting units that will fire within 'num_steps' simulation steps
-
-                spks_tsteps, x_coor, y_coor = firing_times[sample_idx][c], units_ids[0][c], units_ids[1][c]
-
-                batch_sample = [ enumerator for _ in range(len(spks_tsteps)) ]          # within batch sample's index (same for each unit that will spike) - to what sample in the batch each unit belongs to
-
-                coo_[0].extend(batch_sample)                                            # sample dimension indexing
-                coo_[1].extend(spks_tsteps)                                             # tstep dimension indexing
-                coo_[2].extend(x_coor)                                                  # image's grid x-coordinate indexing
-                coo_[3].extend(y_coor)                                                  # image's grid y-coordinate indexing
-
-            indices = torch.tensor(coo_, dtype=torch.int64)
-            values = torch.tensor(np.ones(len(coo_[0])), dtype=torch.float32)
-            
-            X_batch = torch.sparse_coo_tensor(indices, values, (batch_size, num_steps, num_units[0], num_units[0])).to(device=device)
-            Y_batch = torch.tensor(labels_[batch_index], device=device)
-
-            # Uncomment lines bellow to export the "spiking image" for the samples.
-            # w = X_batch.to_dense()
-            # wtolist = w.tolist()
-            # counts = 0
-            # for sample in wtolist:
-            #     counts += 1
-            #     countt = 0
-            #     for tstep in sample:
-            #         print(f'sample # {counts}, tstep: {countt}')
-            #         plt.title(f'sample # {counts}, tstep: {countt}')
-            #         plt.imshow(np.array(tstep), cmap=plt.cm.gray_r)
-            #         plt.axis('off')
-            #         plt.savefig(f'animation/tstep_{countt}.png')
-            #         plt.close()
-            #         countt += 1
-
-            # input('stopped (v2)...')
-
-            yield X_batch.to(device), Y_batch.to(device)
-
-            counter += 1
-
-    sparse_data_gen(x_train, y_train, batch_size, device)
-    
-if __name__ == '__main__':
-    main()
+        counter += 1
--- main-FashionMNIST.py
+++ main-FashionMNIST.py
@@ -7,10 +7,13 @@ Description: trains a CSNN on the Fashion MNIST dataset (converted into spiking 
 import numpy as np
 import matplotlib.pyplot as plt
 
+from snntorch import functional as SF
+
 import torch
 import torchvision
 
 from CSNN import CSNN
+from FMNIST2spikes import sparse_data_gen
 
 def main():
 
@@ -23,7 +26,7 @@ def main():
         
     # --- 1.1. loading Fashion MNIST dataset ---
         
-    batch_size = 1
+    batch_size = 64
     root = 'datasets'
     
     train_dataset = torchvision.datasets.FashionMNIST(root, train=True, transform=None, target_transform=None, download=True)
@@ -32,13 +35,11 @@ def main():
     # --- 1.2. standardizing dataset ---
     # @TODO - flattened here should be removed since first layer will be a convolutional layer.
 
-    x_train = np.array(train_dataset.data, dtype=float)         # flattening image
-    x_train = x_train.reshape(x_train.shape[0], -1)/255         # normalizing values
-    y_train = np.array(train_dataset.targets, dtype=int)
+    x_train = np.array(train_dataset.data, dtype=float)/255             # normalizing pixels
+    y_train = np.array(train_dataset.targets, dtype=float)
 
-    x_test = np.array(test_dataset.data, dtype=float)
-    x_test = x_test.reshape(x_test.shape[0], -1)/255
-    y_test = np.array(test_dataset.targets, dtype=int)
+    x_test = np.array(test_dataset.data, dtype=float)/255
+    y_test = np.array(test_dataset.targets, dtype=float)
 
     print(f'x_train shape: {x_train.shape}')
     print(f'x_test shape: {x_test.shape}')
@@ -47,136 +48,92 @@ def main():
     print(f'y_test shape: {y_test.shape}')
 
     # --- 1.3. analog to spike data ---
-
-    def current2firing_time(x, tau_mem=20.0, thr=0.2, tmax=100, epsilon=1e-7):
-        """
-        Computes first spiking time latency for a current input x ('current injection') fed to a LIF neuron (current-based LIF).
-
-        Arguments:
-        - tau_mem: membrane time constant of LIF neuron.
-        - thr: membrane's spike threshold
-        - tmax: maximum time returned (neurons that did not fire)
-
-        Returns:
-        - T: time to first spike given each "current injection" represented in x.
-        """
-
-        idx = x < thr                           # neurons that did not fire a spike
-        x = np.clip(x, thr+epsilon, 1e9)        # prevents invalid computations in the subsequent logarithmic calculation
-        T = tau_mem*np.log(x/(x-thr))           # time mem takes to reach thr given input 'x' and time constant 'tau' for current-based LIF neuron
-        T[idx] = tmax                           # neurons that did not fire a spike take the longest to first spike
-
-        return  np.array(T, dtype=int)
-
-    def sparse_data_gen(X, Y, batch_size: int, device, shuffle=True, num_units=28*28, time_step=1e-3, num_steps=100, tau_mem=20e-3):
-        """
-        Take a dataset in analog (continuous value) format and generates spikes tensors.
-
-        Arguments:
-        - batch_size: number of sample in each batch
-        - shuffle: shuffle samples during batch creation
-        - num_units: number of units to which data will be fed to
-        - time_step: time taken by a single simulated time step (how finely time is discretized)
-        - num_steps: number of discrete time steps simulated (each representing 'time_step' units of time)
-        - tau_mem: membrane time constant (continuous-time)
-
-        Returns:
-        - X_batch: three dimensional tensor (# of sample in the batch, # of discrete time steps, # of units) containing spike representation of images dataset
-        - Y_batch: the labels
-        """
-
-        labels_ = np.array(Y, dtype=int)
-        num_batches = len(X)//batch_size
-        sample_index = np.arange(len(X))
-
-        if shuffle:
-            np.random.shuffle(sample_index)
-
-        # compute discrete firing times
-            
-        tau_mem_adjusted = tau_mem/time_step    # scaling (continuous-time) 'tau_mem' to fit the simulation's (discrete-time) step
-
-        firing_times = current2firing_time(X, 
-                                           tau_mem=tau_mem_adjusted, 
-                                           tmax=num_steps)
-        units_ids = np.arange(num_units)        
-
-        counter = 0
-
-        while counter < num_batches:                                                    # building i-th batch of spiking data
-
-            batch_index = sample_index[batch_size*counter:batch_size*(counter+1)]       # gathering samples belonging to the i-th batch
-
-            coo_tensor = [ [] for i in range(3) ]                                       # matrix holding analog-latency conversion results of all samples within the i-th batch
-
-            for enumerator, sample_idx in enumerate(batch_index):
-
-                c = firing_times[sample_idx] < num_steps                                # getting units that will fire within 'num_steps' simulation steps
-
-                times, units = firing_times[sample_idx][c], units_ids[c]                # units that will fire and their respective time-to-first-spike 
-
-                batch_sample = [ enumerator for _ in range(len(times)) ]                # within batch sample's index (same for each unit that will spike) - to what sample in the batch each unit belongs to
-
-                coo_tensor[0].extend(batch_sample)                                      # sample index within the batch (same for each unit)
-                coo_tensor[1].extend(times)                                             # time-to-first-spike of each unit that has reached the threshold
-                coo_tensor[2].extend(units)                                             # units' IDs)
-
-            """
-                In the following lines of code are creatig sparse tensors for improved memory usage (source: https://pytorch.org/docs/stable/sparse.html, 
-            section "Construction"). They will effectively transform the latency encoding above into spiking data.
-
-            The tensor 'X_batch' will have three dimensions (a - # of sample in the batch, b - # of discrete time steps, c - # of units). This means that, for each of
-            the 'num_steps' discrete time steps, a unit 'c' in sample 'a' will have a '1' at X_batch[a][b][c] if at time_step 'b' it was above the thr value (it will be
-            '0' otherwise).
-
-            The data becomes "spiking" data because the 2nd argument of 'torch.sparse.FloatTensor' sets the entries (values) for 'X_batch' that are not supposed to be
-            the default (zero) ones.
-
-            The 3rd argument of 'torch.sparse.FloatTensor' defines the dimensions of the resulting tensor (3D in this case, as explined above). This means that each
-            sample (1st dim.) is represented in a span of 'num_steps' (2nd dim.) discrete time by 'num_units' (3rd dim.) spiking units. The computed time-to-first-spike
-            gives in which of the 'num_steps' the unit will be presented as having spiked.
-            """
-
-            i = torch.LongTensor(coo_tensor).to(device)                                 # location for entries that do not have default value (zero by default)
-            v = torch.FloatTensor(np.ones(len(coo_tensor[0]))).to(device)               # entries for locations without default value (setting to one)
-
-            X_batch = torch.sparse.FloatTensor(i, 
-                                               v, 
-                                               torch.Size([batch_size, num_steps, num_units])
-                                               ).to(device)
-            
-            Y_batch = torch.tensor(labels_[batch_index], device=device)
-
-            # Uncomment lines bellow to export the "spiking image" for the samples.
-            # w = X_batch.to_dense()
-            # wtolist = w.tolist()
-            # counts = 0
-            # for sample in wtolist:
-            #     counts += 1
-            #     countt = 0
-            #     for tstep in sample:
-            #         print(f'sample # {counts}, tstep: {countt}')
-            #         plt.title(f'sample # {counts}, tstep: {countt}')
-            #         plt.imshow(np.array(tstep).reshape(28, 28), cmap=plt.cm.gray_r)
-            #         plt.axis('off')
-            #         plt.savefig(f'animation/tstep_{countt}.png')
-            #         plt.close()
-            #         countt += 1
-
-            yield X_batch.to(device), Y_batch.to(device)
-
-            counter += 1
-
-    sparse_data_gen(x_train, y_train, batch_size, device)
         
 
     ### 1. CSNN INSTANTIATION ###
-        
-    num_steps = 50
-    
-    
 
-    
+    num_steps = 50
+
+    net = CSNN(batch_size = batch_size)
+
+    loss_fn = SF.ce_rate_loss()             # cross entropy loss to the output spike count in order train a rate-coded network
+
+    ### 2. TRAINING LOOP ###
+
+    def batch_accuracy(x_, y_, batch_size, device, num_steps = num_steps):
+        '''
+        Returns the accuracy on the entire DataLoader object.
+        '''
+        with torch.no_grad():
+            total = 0
+            acc = 0
+            net.eval()
+
+            for x_local, y_local in sparse_data_gen(x_, y_, batch_size, device, num_steps = num_steps):
+                data = x_local.to_dense().to(device)
+                targets = y_local.to_dense().to(device)
+                spk_rec, _ = CSNN.forward_pass_spikes(net, data, num_steps)
+
+                acc += SF.accuracy_rate(spk_rec, targets) * spk_rec.size(1)
+                total += spk_rec.size(1)
+
+        return acc/total
+
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-2, betas=(0.9, 0.999))
+    num_epochs = 1
+    loss_hist = []
+    test_acc_hist = []
+    counter = 0
+
+    # outer training loop
+    for epoch in range(num_epochs):
+
+        print(f'>> epoch #: {epoch}')
+
+        batch_idx = 0
+
+        # training loop
+        for x_local, y_local in sparse_data_gen(x_train, y_train, batch_size, device, num_steps = num_steps):
+            data = x_local.to_dense().to(device)
+            targets = y_local.to_dense().to(device)
+
+            # forward pass
+            net.train()
+            spk_rec, _ = CSNN.forward_pass_spikes(net, data, num_steps)
+
+            # print(f'       batch: [{batch_idx}]')
+
+            batch_idx += 1
+
+            # initialize the loss & sum over time
+            loss_val = loss_fn(spk_rec, targets.long())
+
+            # gradient calculation + weight update
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+
+            # store loss history for future plotting
+            loss_hist.append(loss_val.item())
+
+            # test set
+            if counter % 50 == 0:
+                with torch.no_grad():
+                    net.eval()
+
+                    # Test set forward pass
+                    test_acc = batch_accuracy(x_test, y_test, batch_size, device, num_steps = num_steps)
+                    print(f"Iteration {counter}, Test Acc: {test_acc * 100:.2f}%\n")
+                    test_acc_hist.append(test_acc.item())
+
+            counter += 1
+
+    fig = plt.figure(facecolor="w")
+    plt.plot(test_acc_hist)
+    plt.title("Test Set Accuracy")
+    plt.xlabel("batch number")
+    plt.ylabel("Accuracy")
+    plt.show()
     
 if __name__ == '__main__':
     main()
